5e5ee2ac0f99ed01ff4bb35f4050a975f5b18f2b
Ritika <35899910+Ritika432@users.noreply.github.com>
2018-02-01 20:56:05 +0530

    Create Prog2.java

---
+++ Prog2.java
@@ -0,0 +1,21 @@
+class fib
+{
+public static void main (String []srgs)
+{
+int a=0,b=1,bo=4000000,s=0,i;
+boolean a=false;
+while (!=a)
+{
+i=a+b;
+a=b;
+b=i;
+if (i>bo)
+{
+a=true;
+}
+if (i%2==0)
+{
+s=s+i;
+}}
+System.out.println (s);
+}}
