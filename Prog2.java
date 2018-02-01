5e04e754ecbd1756ca448aa84adc3554d6d729b7
Ritika <35899910+Ritika432@users.noreply.github.com>
2018-02-01 23:07:02 +0530

    Update Prog2.java

--- Prog2.java
+++ Prog2.java
@@ -3,15 +3,15 @@ class Prog2
 public static void main (String []srgs)
 {
 int a=0,b=1,bo=4000000,s=0,i;
-boolean a=false;
-while (a=!a)
+boolean c=false;
+while (!c)
 {
 i=a+b;
 a=b;
 b=i;
 if (i>bo)
 {
-a=true;
+c=true;
 }
 if (i%2==0)
 {
