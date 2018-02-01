b50c91076bdffb353e03167a510de31136ee4955
Ritika <35899910+Ritika432@users.noreply.github.com>
2018-02-01 20:55:03 +0530

    Create Prog1.java

---
+++ Prog1.java
@@ -0,0 +1,17 @@
+class new
+{
+public static void main (String args[])
+{
+System.out.println ("sum="+getsum());
+}
+public static int getsum()
+{
+int s=0;
+for(int i=1;i<1000;i++)
+{
+if (i%3==0||i%5==0)
+{
+s=s+i;
+}}
+return sum;
+}
