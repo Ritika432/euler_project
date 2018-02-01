e90d92c7411f2b4ee6a85de50e7c0f6fb78f535b
Ritika <35899910+Ritika432@users.noreply.github.com>
2018-02-01 21:06:05 +0530

    Create Prog5.java

---
+++ Prog5.java
@@ -0,0 +1,25 @@
+class Prog5{
+public static void main (String[]args)
+{
+mul();
+}
+public static int mul()
+{
+boolean a=false;
+boolean c=true;
+int b=2520;
+while(!a)
+{
+b=b+2520;
+for (int i=1;i<=20;i++)
+{
+if (i%j!=0)
+c=false;
+break;
+}if(c)
+{
+a=true;
+}
+}
+return b;
+}}
