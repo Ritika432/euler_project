62d03b3e94509b98b71b335646e04c305b33874c
Ritika <35899910+Ritika432@users.noreply.github.com>
2018-02-05 18:59:11 +0530

    Update Prog4.java

--- Prog4.java
+++ Prog4.java
@@ -1,26 +1,22 @@
-class Prog4{
-public static void main (String[]args)
-{
+public static void main(String[] args) {
 long p;
-for(int i=100;i<=999;i++)
-{
-for (j=100;j<=999;j++)
-{
-p=i*j;
-if (reverse(p))
-System.out.println (p);
-break;
-}}}
-public static reverse(long l)
-{
-long rev=0;
-while(l!=0)
-{
-int d=l%10;
-r=(r*10)+d;
-l=l/10;
+    for (int i = 999; i >= 100; i--) {
+        for (int j = 999; i >= 100; i--) {
+            p= j * i;
+            if (reverse(p)) {
+                System.out.println(p);
+                break;
+            }
+        }
+    }
 }
-return (r==l)
-}
-}
-}
+
+
+private static final boolean reverse(long v) {
+    long r = 0;
+    while (v != 0) {
+        r *= 10;
+        r += v % 10;
+        v /= 10;
+    }
+    return (r == v);
